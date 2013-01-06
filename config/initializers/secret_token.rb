21c055c7c365941051400c66f4575c12b69751c5
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-01-06 18:45:08 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tkrk10Server::Application.config.secret_token = '61e2a8640fccdbcc5d118a1b06f5d07532c653e0f37eb91c8650d413e8ca088fa07ff1436f9cfaf625b0fa826731be1eb456b7e23e70db210f7af2f69f7d9618'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tkrk10Server::Application.config.session_store :cookie_store, key: '_tkrk10_server_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tkrk10Server::Application.config.session_store :active_record_store
